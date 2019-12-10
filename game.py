792acad0982977b8a64fa6c6d953d53ca48b69b9
cosmicomic <liang.cjs@gmail.com>
2019-12-09 20:54:38 -0800

    start representing with time steps

--- game.py
+++ game.py
@@ -5,19 +5,22 @@ import random
 import time
 from pygame.math import Vector2
 
-agent_speed = 3
-player_speed = 3
-player_velocity = [0, 0]
+# Game parameters
 game_height = 400
 game_width = 600
-num_agents = 4
+agent_speed = 5
+player_velocity = Vector2(0, 0)
+player_position = [0, 0]
+num_agents = 1
+dt = 0.5
+player_accel = 0.3
 
 def generate_position():
-    return (random.random() * game_width, random.random() * game_height)
+    return Vector2(random.random() * game_width, random.random() * game_height)
 
 def generate_velocity():
     angle = (2 * pi) * random.uniform(-1, 1)
-    return [agent_speed * cos(angle), agent_speed * sin(angle)]
+    return Vector2(agent_speed * cos(angle), agent_speed * sin(angle))
 
 class Circle:
     def __init__(self):
@@ -25,21 +28,35 @@ class Circle:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(current_dir, "agent.png")
 
+        self.accel = 0.5
+        self.max_velocity = 10
+
         # initialize position, velocity, and rect object
         self.position = generate_position()
         self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect(center=self.position)
         self.velocity = generate_velocity()
 
-    def update(self):
-        self.rect = self.rect.move(self.velocity)
-        self.update_velocity()
+    def update(self, dt, u):
+        self.position[0] += self.velocity.x * dt
+        self.position[1] += self.velocity.y * dt
 
-    def update_velocity(self):
-        if self.rect.left < 0 or self.rect.right > game_width:
-            self.velocity[0] = -self.velocity[0]
-        if self.rect.top < 0 or self.rect.bottom > game_height:
-            self.velocity[1] = -self.velocity[1]
+        self.velocity.x += dt*u[0]
+        self.velocity.y += dt*u[1]
+        self.velocity.x = max(-self.max_velocity,
+                              min(self.velocity.x, self.max_velocity))
+        self.velocity.y = max(-self.max_velocity,
+                              min(self.velocity.y, self.max_velocity))
+
+    def get_acceleration(self):
+        if self.position.x < 0 or (self.position.x + self.rect.width) > game_width:
+            return [-2 * self.velocity[0], 0]
+        if self.position.y < 0 or (self.position.y + self.rect.height) > game_height:
+            return [0, -2 * self.velocity[1]]
+
+        # no wall collision; don't change velocity
+        return [0, 0]
+
 
 class Game:
     def __init__(self):
@@ -64,11 +81,11 @@ class Game:
     
     def update_agents(self):
         for agent in self.agents:
-            agent.update()
+            agent.update(dt, agent.get_acceleration())
 
     def render_agents(self):
         for agent in self.agents:
-            self.screen.blit(agent.image, agent.rect)
+            self.screen.blit(agent.image, agent.position)
 
     def run(self):
         self.generate_agents(num_agents)
@@ -90,7 +107,7 @@ class Game:
         textRect = text.get_rect()
 
         while not self.exit:
-            
+        
             # Event queue
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -99,28 +116,32 @@ class Game:
             if self.collisions >= self.max_collisions:
                 self.exit = True
 
-            if goal_rect.contains(player_rect):
-                self.exit = True
+            # if goal_rect.contains(player_rect):
+            #     self.exit = True
+
 
             self.update_agents()
-
-            # Update player position and velocity
-            player_rect = player_rect.move(player_velocity)
 
             # User input
             pressed = pygame.key.get_pressed()
 
-            if pressed[pygame.K_UP] and not (player_rect.top < 0):
-                player_velocity[1] = -player_speed
-            elif pressed[pygame.K_DOWN] and not (player_rect.bottom > self.height):
-                player_velocity[1] = player_speed
-            elif pressed[pygame.K_RIGHT] and not (player_rect.right > self.width):
-                player_velocity[0] = player_speed
-            elif pressed[pygame.K_LEFT] and not (player_rect.left < 0):
-                player_velocity[0] = -player_speed
+            if pressed[pygame.K_UP] and not (player_position[1] < 0):
+                player_velocity.y -= player_accel
+            elif pressed[pygame.K_DOWN] and not (player_position[1] + player_rect.height > self.height):
+                player_velocity.y += player_accel
+            elif pressed[pygame.K_RIGHT] and not (player_position[0] + player_rect.width > self.width):
+                player_velocity.x += player_accel
+            elif pressed[pygame.K_LEFT] and not (player_position[0] < 0):
+                player_velocity.x -= player_accel
             else:
-                player_velocity[0] = 0
-                player_velocity[1] = 0
+                player_velocity.x = 0
+                player_velocity.y = 0
+            
+            # Update user position
+            player_position[0] += int(player_velocity.x * dt)
+            player_position[1] += int(player_velocity.y * dt)
+            player_rect.move(int(player_velocity.x * dt), int(player_velocity.y * dt))
+            print(player_rect.x, player_rect.y)
 
             # check for collisions
             if player_rect.collidelist([agent.rect for agent in self.agents]) != -1:
@@ -135,7 +156,8 @@ class Game:
             self.screen.fill((255, 255, 255))
             self.screen.blit(goal_image, goal_rect)
             self.render_agents()
-            self.screen.blit(player_image, player_rect)
+            # self.screen.blit(player_image, player_rect)
+            self.screen.blit(player_image, player_position)
             self.screen.blit(text, textRect) 
             pygame.display.flip()
 
